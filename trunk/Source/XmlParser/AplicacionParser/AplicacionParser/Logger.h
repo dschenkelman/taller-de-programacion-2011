b5bcb753be0202775a6baec5446997ab87538798
juanma.aguero@gmail.com <juanma.aguero@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-03-28 00:06:47 +0000

    Logger funcionando. Se agregaron otros contructores y pruebas.

--- trunk/Source/XmlParser/AplicacionParser/AplicacionParser/Logger.h
+++ trunk/Source/XmlParser/AplicacionParser/AplicacionParser/Logger.h
@@ -11,6 +11,7 @@ private:
 	const char* mensajeError;
 	void escribirEnArchivo(const char* mensaje);
 public:
+	Logger(const char* ruta);
 	Logger(const char* ruta, const char* mensajeWarning, const char* mensajeError);
 	~Logger(void);
 	void logWarning(const char* mensaje);
