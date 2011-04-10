be2da0ccdaa7c5be19e58b34c8ee4bf5ad37822b
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-04-10 23:08:50 +0000

    # Agregue error para tipo obstaculo

--- Source/XmlParser/AplicacionParser/AplicacionParser/TipoObstaculo.h
+++ Source/XmlParser/AplicacionParser/AplicacionParser/TipoObstaculo.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "XmlElement.h"
+#include <string>
 
 class TipoObstaculo
 {
 	std::string nombre;
+	List<std::string> validAttributes;
+	bool tieneError;
 	char textura;
 	bool utilizado;
+	void populateValidAttributes(void);
+	bool validateAttributes(XmlElement& e);
 public:
 	TipoObstaculo(XmlElement& e);
 	TipoObstaculo(std::string& n, char t);
@@ -13,6 +18,7 @@ public:
 	void utilizarTipo();
 	bool esUtilizado();
 	std::string getNombre();
+	bool hasError(void);
 	char getTextura();
 	~TipoObstaculo(void);
 };
