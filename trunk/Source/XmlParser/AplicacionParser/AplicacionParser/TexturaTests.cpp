55f7395af4767d6c90d25e5f654cfbff64735954
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-04-21 21:10:58 +0000

    # Adding functionality to Textura

--- trunk/Source/XmlParser/AplicacionParser/AplicacionParser/TexturaTests.cpp
+++ trunk/Source/XmlParser/AplicacionParser/AplicacionParser/TexturaTests.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "Textura.h"
 #include "Logger.h"
+#include <limits>
 
 //useful to detect memory leaks
 #define _CRTDBG_MAP_ALLOC
@@ -32,7 +33,12 @@ void TexturaTests::run()
 	printResult("testHasErrorIfAttributeNameIsNotValid", testHasErrorIfAttributeNameIsNotValid());
 	printResult("testHasErrorIfItDoesNotHaveArchivoAttribute", testHasErrorIfItDoesNotHaveArchivoAttribute());
 	printResult("testHasErrorIfItDoesNotHaveNombreAttribute", testHasErrorIfItDoesNotHaveNombreAttribute());
-	
+	printResult("testBoundsAreZeroAndMaxIntWhenNotSpecified", testBoundsAreZeroAndMaxIntWhenNotSpecified());
+	printResult("testBoundsAreCorrectWhenSpecified", testBoundsAreCorrectWhenSpecified());
+	printResult("testBoundStringValuesAreNotConsidered", testBoundStringValuesAreNotConsidered());
+	printResult("testBoundNegativeValuesAreNotConsidered", testBoundNegativeValuesAreNotConsidered());
+	printResult("testRightOrBottomLowerThanLeftOrTopUseDefaults", testRightOrBottomLowerThanLeftOrTopUseDefaults());
+
 	int leaks = _CrtDumpMemoryLeaks();
 	printLeaks(leaks);
 }
@@ -41,14 +47,14 @@ bool TexturaTests::testIsCreatedFromElementWithNameTextura(void)
 {
 	XmlElement elemento("Textura", 1, 100);
 	XmlAttribute atributo1("nombre", "Frutilla");
-	XmlAttribute atributo2("archivo", "Images/Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
 	elemento.addAttribute(atributo1);
 	elemento.addAttribute(atributo2);
 
 	Textura textura(elemento);
 
 	bool successCondition1 = textura.getNombre() == "Frutilla";
-	bool successCondition2 = textura.getArchivo() == "Images/Frutilla";
+	bool successCondition2 = textura.getPath() == "Images/Frutilla";
 	bool successCondition3 = !textura.hasError();
 
 	return successCondition1 && successCondition2 && successCondition3;
@@ -58,7 +64,7 @@ bool TexturaTests::testHasErrorIfAttributeNameIsNotValid(void)
 {
 	XmlElement elemento("Textura", 1, 100);
 	XmlAttribute atributo1("nombre", "Frutilla");
-	XmlAttribute atributo2("archivo", "Images/Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
 	XmlAttribute atributo3("incorrecto", "incorrecto");
 	elemento.addAttribute(atributo1);
 	elemento.addAttribute(atributo2);
@@ -90,16 +96,148 @@ bool TexturaTests::testHasErrorIfItDoesNotHaveArchivoAttribute(void)
 bool TexturaTests::testHasErrorIfItDoesNotHaveNombreAttribute(void)
 {
 	XmlElement elemento("Textura", 1, 100);
-	XmlAttribute atributo1("archivo", "Images/Frutilla");
+	XmlAttribute atributo1("path", "Images/Frutilla");
 	elemento.addAttribute(atributo1);
 
 	Textura textura(elemento);
 	bool successCondition1 = textura.hasError();
-	bool successCondition2 = textura.getArchivo() == "Images/Frutilla";
+	bool successCondition2 = textura.getPath() == "Images/Frutilla";
 
 	Logger::closeLog();
 
 	return successCondition1 && successCondition2;
+}
+
+bool TexturaTests::testBoundsAreZeroAndMaxIntWhenNotSpecified(void)
+{
+	XmlElement elemento("Textura", 1, 100);
+	XmlAttribute atributo1("nombre", "Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
+	elemento.addAttribute(atributo1);
+	elemento.addAttribute(atributo2);
+
+	Textura textura(elemento);
+
+	bool successCondition1 = textura.getLeft() == 0;
+	bool successCondition2 = textura.getTop() == 0;
+	bool successCondition3 = textura.getRight() == numeric_limits<int>::max();
+	bool successCondition4 = textura.getBottom() == numeric_limits<int>::max();
+	bool successCondition5 = !textura.hasError();
+
+	return successCondition1 && successCondition2 &&  
+		successCondition3 && successCondition4 && successCondition5;
+}
+
+bool TexturaTests::testBoundsAreCorrectWhenSpecified(void)
+{
+	XmlElement elemento("Textura", 1, 100);
+	XmlAttribute atributo1("nombre", "Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
+	XmlAttribute atributo3("top", "100");
+	XmlAttribute atributo4("left", "200");
+	XmlAttribute atributo5("right", "300");
+	XmlAttribute atributo6("bottom", "400");
+	elemento.addAttribute(atributo1);
+	elemento.addAttribute(atributo2);
+	elemento.addAttribute(atributo3);
+	elemento.addAttribute(atributo4);
+	elemento.addAttribute(atributo5);
+	elemento.addAttribute(atributo6);
+
+	Textura textura(elemento);
+
+	bool successCondition1 = textura.getLeft() == 200;
+	bool successCondition2 = textura.getTop() == 100;
+	bool successCondition3 = textura.getRight() == 300;
+	bool successCondition4 = textura.getBottom() == 400;
+	bool successCondition5 = !textura.hasError();
+
+	return successCondition1 && successCondition2 &&  
+		successCondition3 && successCondition4 && successCondition5;
+}
+
+bool TexturaTests::testBoundStringValuesAreNotConsidered(void)
+{
+	XmlElement elemento("Textura", 1, 100);
+	XmlAttribute atributo1("nombre", "Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
+	XmlAttribute atributo3("top", "A");
+	XmlAttribute atributo4("left", "B");
+	XmlAttribute atributo5("right", "C");
+	XmlAttribute atributo6("bottom", "D");
+	elemento.addAttribute(atributo1);
+	elemento.addAttribute(atributo2);
+	elemento.addAttribute(atributo3);
+	elemento.addAttribute(atributo4);
+	elemento.addAttribute(atributo5);
+	elemento.addAttribute(atributo6);
+
+	Textura textura(elemento);
+	
+	bool successCondition1 = textura.getLeft() == 0;
+	bool successCondition2 = textura.getTop() == 0;
+	bool successCondition3 = textura.getRight() == numeric_limits<int>::max();
+	bool successCondition4 = textura.getBottom() == numeric_limits<int>::max();
+	bool successCondition5 = !textura.hasError();
+
+	return successCondition1 && successCondition2 &&  
+		successCondition3 && successCondition4 && successCondition5;
+}
+
+bool TexturaTests::testBoundNegativeValuesAreNotConsidered(void)
+{
+	XmlElement elemento("Textura", 1, 100);
+	XmlAttribute atributo1("nombre", "Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
+	XmlAttribute atributo3("top", "-100");
+	XmlAttribute atributo4("left", "-200");
+	XmlAttribute atributo5("right", "-300");
+	XmlAttribute atributo6("bottom", "-400");
+	elemento.addAttribute(atributo1);
+	elemento.addAttribute(atributo2);
+	elemento.addAttribute(atributo3);
+	elemento.addAttribute(atributo4);
+	elemento.addAttribute(atributo5);
+	elemento.addAttribute(atributo6);
+
+	Textura textura(elemento);
+	
+	bool successCondition1 = textura.getLeft() == 0;
+	bool successCondition2 = textura.getTop() == 0;
+	bool successCondition3 = textura.getRight() == numeric_limits<int>::max();
+	bool successCondition4 = textura.getBottom() == numeric_limits<int>::max();
+	bool successCondition5 = !textura.hasError();
+
+	return successCondition1 && successCondition2 &&  
+		successCondition3 && successCondition4 && successCondition5;
+}
+
+bool TexturaTests::testRightOrBottomLowerThanLeftOrTopUseDefaults(void)
+{
+	XmlElement elemento("Textura", 1, 100);
+	XmlAttribute atributo1("nombre", "Frutilla");
+	XmlAttribute atributo2("path", "Images/Frutilla");
+	XmlAttribute atributo3("top", "100");
+	XmlAttribute atributo4("left", "200");
+	XmlAttribute atributo5("right", "150");
+	XmlAttribute atributo6("bottom", "50");
+	elemento.addAttribute(atributo1);
+	elemento.addAttribute(atributo2);
+	elemento.addAttribute(atributo3);
+	elemento.addAttribute(atributo4);
+	elemento.addAttribute(atributo5);
+	elemento.addAttribute(atributo6);
+
+	Textura textura(elemento);
+	
+	bool successCondition1 = textura.getLeft() == 200;
+	bool successCondition2 = textura.getTop() == 100;
+	bool successCondition3 = textura.getRight() == numeric_limits<int>::max();
+	bool successCondition4 = textura.getBottom() == numeric_limits<int>::max();
+	bool successCondition5 = !textura.hasError();
+
+	return successCondition1 && successCondition2 &&  
+		successCondition3 && successCondition4 && successCondition5;
 }
 
 TexturaTests::~TexturaTests(void)
