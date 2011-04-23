b65885ea458feecbbcc799c33b9c5806791c1ecb
juanma.aguero@gmail.com <juanma.aguero@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-04-23 22:15:09 +0000

    Se corrige el interpolado.

--- Source/XmlParser/AplicacionParser/AplicacionParser/ImageFx.cpp
+++ Source/XmlParser/AplicacionParser/AplicacionParser/ImageFx.cpp
@@ -9,7 +9,7 @@ Image* ImageFx::resize(Image *imgSrc, int widthDst, int heightDst){
 	// Empty new image
 	Image* imgDst = new Image(widthDst, heightDst);
 	
-	int stepsX = 0;
+	
 	for(int posYSrc = 0; posYSrc < height ; posYSrc++){
 		
 		int newY = (posYSrc * heightDst) / height;
@@ -20,20 +20,21 @@ Image* ImageFx::resize(Image *imgSrc, int widthDst, int heightDst){
 
 			int newX = (posXSrc * widthDst) / width;
 			int antX = ((posXSrc-1) * widthDst) / width;
+			int proX = ((posXSrc+1) * widthDst) / width;
+			int stepsX = (proX-newX)/2;
 			if( posXSrc != 0 && widthDst > width ){
 				
 				// obtengo el pixel anterior
-				Uint32 pixelAnt = imgSrc->getPixel(antX, newY);
-				
+				Uint32 pixelAnt = imgSrc->getPixel((posXSrc-1), posYSrc);
+				stepsX = newX-antX;
 				// Obtengo las coordenadas rgb del pixel
 				Uint8 antR, antG, antB, futR, futG, futB;
 				SDL_GetRGB(pixelAnt, imgSrc->getFormat(), &antR, &antG, &antB);
 				SDL_GetRGB(pixelImg, imgSrc->getFormat(), &futR, &futG, &futB);
-				stepsX = (newX-antX);
 				int range = stepsX-1;
 				
 				// Interpolo linealmente para obtener las nuevas coordenadas
-				for (int i=1; i < stepsX; i++)
+				for (int i=0; i < range; i++)
 				{
 					int j = range - i;
 					int iR = ((antR * j) + (futR * i)) / range;
@@ -44,14 +45,15 @@ Image* ImageFx::resize(Image *imgSrc, int widthDst, int heightDst){
 					Uint32 pixelFut = SDL_MapRGB(imgDst->getFormat(), iR, iG, iB);
 					
 					// Coloco el pixel interpolado
-					imgDst->putPixel( pixelFut,(antX+i), newY );
+					imgDst->putPixel( pixelFut,(antX+1+i), newY );
 				}
 			}
 
 			if(posYSrc != 0 && heightDst > height ){
+				
 				int antY = ((posYSrc-1) * heightDst) / height;
 				// obtengo el pixel anterior
-				Uint32 pixelAnt = imgSrc->getPixel(newX, antY);
+				Uint32 pixelAnt = imgSrc->getPixel(posXSrc, (posYSrc-1));
 				
 				// Obtengo las coordenadas rgb del pixel
 				Uint8 antR, antG, antB, futR, futG, futB;
@@ -72,8 +74,15 @@ Image* ImageFx::resize(Image *imgSrc, int widthDst, int heightDst){
 					Uint32 pixelFut = SDL_MapRGB(imgDst->getFormat(), iR, iG, iB);
 					
 					// Coloco el pixel interpolado
-					for(int x = 0; x<stepsX; x++){
-						imgDst->putPixel( pixelFut, (antX+1+x), (antY+i));
+					if(posXSrc == 0){
+						int hastaX = (stepsX/2);
+						for(int k = 0; k<hastaX; k++){
+							imgDst->putPixel( pixelFut, k, (antY+i));
+						}
+					}else{
+						for(int k = (antX+1); k<=newX; k++){
+							imgDst->putPixel( pixelFut, k, (antY+i));
+						}
 					}
 				}
 			}
@@ -82,6 +91,6 @@ Image* ImageFx::resize(Image *imgSrc, int widthDst, int heightDst){
 			imgDst->putPixel( pixelImg, newX, newY);
 		}
 	}
-
 	return imgDst;
+
 }
