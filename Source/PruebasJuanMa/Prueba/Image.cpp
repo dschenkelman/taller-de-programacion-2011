6a753ea1d9cf6604f81218146bfe1008789d4641
juanma.aguero@gmail.com <juanma.aguero@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-04-23 14:29:24 +0000

    Agrego getPixel y putPixel a Image. Agrego constructor de imagen vacia.

--- Source/PruebasJuanMa/Prueba/Image.cpp
+++ Source/PruebasJuanMa/Prueba/Image.cpp
@@ -3,6 +3,7 @@
 #include "Window.h"
 #include "sdl/sdl.h"
 
+
 /*
  * Return the pixel value at (x, y)
  * NOTE: The surface must be locked before calling this!
@@ -87,6 +88,82 @@ Image::Image(char* uri)
 Image::Image(SDL_Surface* surface)
 {
 	this->surface = surface;
+}
+
+
+/** Empty Image constructor */
+Image::Image(int height, int width){
+	this->surface = SDL_CreateRGBSurface(SDL_SWSURFACE,height,width,24,0,0,0,0);
+}
+
+
+int Image::getHeight(){
+	return this->surface->h;
+}
+
+
+int Image::getWidth(){
+	return this->surface->w;
+}
+
+
+/** Get image's pixel at x and y */
+Uint32 Image::getPixel(int x, int y){
+    int bpp = this->surface->format->BytesPerPixel;
+    Uint8 *p = (Uint8 *)this->surface->pixels + y * this->surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+
+    case 2:
+        return *(Uint16 *)p;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+
+    case 4:
+        return *(Uint32 *)p;
+
+    default:
+        return 0;
+    }
+}
+
+
+/** Put a pixel in this image at x and y */
+void Image::putPixel(Uint32 pixel, int x, int y){
+    int bpp = this->surface->format->BytesPerPixel;
+    Uint8 *p = (Uint8 *)this->surface->pixels + y * this->surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        *p = pixel;
+        break;
+
+    case 2:
+        *(Uint16 *)p = pixel;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        } else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32 *)p = pixel;
+        break;
+    }
 }
 
 void Image::paste(SDL_Surface* board, int x, int y)
--- Source/PruebasJuanMa/Prueba/Image.h
+++ Source/PruebasJuanMa/Prueba/Image.h
@@ -6,13 +6,24 @@
 class Image
 {
 public:
+	/** Constructors */
 	//Image(void);
 	Image(char* uri);
 	//Image(String uri, int x, int y, int width, int height);
+	Image(int height, int width); // Empty image constructor
+	
 	void paste(SDL_Surface* board, int x, int y);
 	void paste(Window board, int x, int y);
 	void cropAndPaste(int x0, int y0, int width, int height, int x1, int y2, SDL_Surface* board);
 	~Image(void);
+	int getHeight();
+	int getWidth();
+	
+	/** Get image's pixel at x and y */
+	Uint32 getPixel(int x, int y);
+	
+	/** Put a pixel in this image at x and y */
+	void putPixel(Uint32 pixel, int x, int y);
 private:
 	Image(SDL_Surface* surface);
 	SDL_Surface* surface;
