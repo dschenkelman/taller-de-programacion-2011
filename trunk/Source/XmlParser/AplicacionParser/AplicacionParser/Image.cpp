1a378026400f3df737e07f1cb11901732cb6d3fd
juanma.aguero@gmail.com <juanma.aguero@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-05-07 10:36:47 +0000

    Volvi a usar el Pitch en el getPixel y putPixel.

--- trunk/Source/XmlParser/AplicacionParser/AplicacionParser/Image.cpp
+++ trunk/Source/XmlParser/AplicacionParser/AplicacionParser/Image.cpp
@@ -78,8 +78,8 @@ Image::~Image(void)
 
 /** Get image's pixel at x and y */
 Uint32 Image::getPixel(int x, int y) const{
-    int bpp = this->image->format->BytesPerPixel;
-    Uint8 *p = (Uint8 *)this->image->pixels + ((this->image->w * y + x) * bpp);
+	int bpp = this->image->format->BytesPerPixel;
+    Uint8 *p = (Uint8 *)this->image->pixels + y * this->image->pitch + x * bpp;
 
     switch(bpp) {
     case 1:
@@ -98,7 +98,7 @@ Uint32 Image::getPixel(int x, int y) const{
         return *(Uint32 *)p;
 
     default:
-        return 0;
+        return 0;       
     }
 }
 
@@ -112,36 +112,33 @@ void Image::putPixel(Uint32 pixel, int x, int y)
 void Image::putPixel(SDL_Surface *surface, Uint32 pixel, int x, int y)
 {
 	int bpp = surface->format->BytesPerPixel;
-    Uint8 *p = (Uint8 *)surface->pixels + ((surface->w * y + x) * bpp);
+   
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 
-    switch(bpp) 
-	{
-		case 1:
-			*p = pixel;
-			break;
+    switch(bpp) {
+    case 1:
+        *p = pixel;
+        break;
 
-		case 2:
-			*(Uint16 *)p = pixel;
-			break;
+    case 2:
+        *(Uint16 *)p = pixel;
+        break;
 
-		case 3:
-			if(SDL_BYTEORDER == SDL_BIG_ENDIAN) 
-			{
-				p[0] = (pixel >> 16) & 0xff;
-				p[1] = (pixel >> 8) & 0xff;
-				p[2] = pixel & 0xff;
-			}
-			else 
-			{
-				p[0] = pixel & 0xff;
-				p[1] = (pixel >> 8) & 0xff;
-				p[2] = (pixel >> 16) & 0xff;
-			}
-			break;
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        } else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
 
-		case 4:
-			*(Uint32 *)p = pixel;
-			break;
+    case 4:
+        *(Uint32 *)p = pixel;
+        break;
     }
 }
 
