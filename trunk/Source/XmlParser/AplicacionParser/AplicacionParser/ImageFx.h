f7bded442bbf3fd82e0eaa238a244111e878ecfc
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-05-11 04:38:40 +0000

    # Borro archivos que no estamos usando

--- trunk/Source/XmlParser/AplicacionParser/AplicacionParser/ImageFx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Image.h"
-
-class ImageFx
-{
-public:
-	static Image* resize(Image* imgSrc, int widthDst, int heightDst);
-	static Image* rotate(Image* imgSrc, int grades);
-};
