b73d6988e3c386d21e2920e03c3b9035851993a7
aletorrado@gmail.com <aletorrado@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-05-22 18:53:39 +0000

    Pruebas de base de datos :D

--- Source/sqlite_test/sqlite_test/sqlite_test.cpp
+++ Source/sqlite_test/sqlite_test/sqlite_test.cpp
@@ -1,15 +1,22 @@
+#include "database.h"
+#include "query.h"
 #include "sqlite_test.h"
-#include "sqlite/sqlite3.h"
 #include <iostream>
 using namespace std;
 
 int main() {
 	cout << "hello world!" << endl;
-	sqlite3* testdb;
-	char* testdb_err;
-	sqlite3_open("testdb.db", &testdb);
-	sqlite3_exec(testdb, "create table 'testtable' (id integer);", NULL, 0, &testdb_err);
-	sqlite3_close(testdb);
+	remove("test.sql");
+	database db("test.sql");
+	char* res;
+	res = db.execute("CREATE TABLE IF NOT EXISTS table0 (col0 INTEGER, col1 INTEGER)");
+	if (res) cout << res << endl;
+	res = db.execute("INSERT INTO table0 VALUES (123, 456)");
+	if (res) cout << res << endl;
+	query q = db.getQuery("SELECT * FROM table0");
+	int n0 = q.getValue(1);
+	cout << n0 << endl;
+	char c = getchar();
 	return 0;
 }
 
