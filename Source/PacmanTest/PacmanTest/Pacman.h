5726f273afd4b24b4a2485002d06a489eaa06794
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-05-22 19:32:24 +0000

    # Added test with two pacmans

--- Source/PacmanTest/PacmanTest/Pacman.h
+++ Source/PacmanTest/PacmanTest/Pacman.h
@@ -15,6 +15,10 @@ class Pacman
 	};
 	Image* textura;
 	Direction dir;
+	SDLKey upKey;
+	SDLKey downKey;
+	SDLKey leftKey;
+	SDLKey rightKey;
 	int x;
 	int y;
 	int xDirection;
@@ -22,11 +26,16 @@ class Pacman
 	int screenWidth;
 	int screenHeight;
 	static const int speed = 5;
+	void moveLeft(void);
+	void moveRight(void);
+	void moveDown(void);
+	void moveUp(void);
 public:
-	Pacman(std::string pathTextura, int h, int w);
+	Pacman(std::string pathTextura, int h, int w, int x, int y);
 	Image* getImage(void);
 	void updatePosition(void);
 	void handleKeyStroke(void);
+	void changeKeyboardMappings(SDLKey up, SDLKey down, SDLKey left, SDLKey right);
 	int getX()
 	{
 		return this->x;
--- Source/PacmanTest/PacmanTest/ScreenManager.cpp
+++ Source/PacmanTest/PacmanTest/ScreenManager.cpp
@@ -10,22 +10,36 @@ ScreenManager::ScreenManager(Window* w, string pathFondo)
 {
 	this->window = w;
 	this->fondo = new Image(pathFondo);
-	this->pacman1 = new Pacman("Images/pacman.bmp", this->window->getHeight(), this->window->getWidth());
+	this->pacman1 = new Pacman("Images/pacman.bmp", 
+		this->window->getHeight(), this->window->getWidth(),
+		0, 0);
+
+	this->pacman2 = new Pacman("Images/pacman2.bmp", 
+		this->window->getHeight(), this->window->getWidth(),
+		0, this->window->getHeight() - (this->pacman1->getImage()->getHeight() + 1));
+
+	this->pacman2->changeKeyboardMappings(SDLK_w, SDLK_s, SDLK_a, SDLK_d);
 }
 
 void ScreenManager::handleKeyStroke(void)
 {
 	this->pacman1->handleKeyStroke();
+	this->pacman2->handleKeyStroke();
 }
 
 void ScreenManager::updateScreen(void)
 {
 	this->pacman1->updatePosition();
-	int x = this->pacman1->getX();
-	int y = this->pacman1->getY();
-	Image* i = this->pacman1->getImage();
+	this->pacman2->updatePosition();
+	int x1 = this->pacman1->getX();
+	int y1 = this->pacman1->getY();
+	int x2 = this->pacman2->getX();
+	int y2 = this->pacman2->getY();
+	Image* i1 = this->pacman1->getImage();
+	Image* i2 = this->pacman2->getImage();
 	this->window->display(this->fondo, 0, 0, 0, 0, 0, -1);
-	this->window->display(i, x, y, 255, 255, 255, 10);
+	this->window->display(i1, x1, y1, 255, 255, 255, 10);
+	this->window->display(i2, x2, y2, 255, 255, 255, 10);
 	this->window->refresh();
 }
 
