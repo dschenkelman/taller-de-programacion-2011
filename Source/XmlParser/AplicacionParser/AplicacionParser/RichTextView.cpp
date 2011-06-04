fd95267420cc420d830ca56a1932c5b20dc6d231
juanma.aguero@gmail.com <juanma.aguero@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-06-04 03:22:00 +0000

    Se agregan simbolos: dos puntos, guion medio y guion bajo.

--- Source/XmlParser/AplicacionParser/AplicacionParser/RichTextView.cpp
+++ Source/XmlParser/AplicacionParser/AplicacionParser/RichTextView.cpp
@@ -12,26 +12,41 @@ RichTextView::RichTextView(std::string txt, std::string type, int r, int g, int 
 
 
 RichTextView::RichTextView(std::string txt, std::string type): View(){
-	
+	int letterCount = 0;
 	int letterWidth		= 17;
 	int letterHeight	= 17;
 	
 	// nueva imagen
-	Image* aux = new Image((letterWidth*txt.length()), letterHeight);
+	Image* aux = new Image((letterWidth*this->getLetterCount(txt)), letterHeight);
 
 	// recorro el texto a renderizar
-	for(size_t i=0; i < txt.length(); i++){
+	size_t i=0;
+	while( i < txt.length() ){
+		char c = txt.at(i);
+		std::string letterName = "";
+		if(c == '&'){
+			size_t k = i+1;
+			while( k < txt.length() && txt.at(k) != ';'){
+				letterName += txt.at(k);
+				k++;
+			}
+			i=k;
+		}else{
+			letterName += c;
+		}
 		
-		Image* letter = new Image("images/text/" + type + "/" + txt.at(i) + ".bmp");
+		Image* letter = new Image("images/text/" + type + "/" + letterName + ".bmp");
 		
 		SDL_Rect dst;
 		dst.h=letterHeight;
 		dst.w=letterWidth;
-		dst.x=(letterWidth*i);
+		dst.x=(letterWidth*letterCount);
 		dst.y=0;
 		
 		SDL_BlitSurface(letter->getSDLSurface(), NULL, aux->getSDLSurface(), &dst);
 		delete letter;
+		i++;
+		letterCount++;
 	}
 	this->copy(*aux);
 
@@ -39,3 +54,22 @@ RichTextView::RichTextView(std::string txt, std::string type): View(){
 }
 
 RichTextView::~RichTextView(){}
+
+int RichTextView::getLetterCount(std::string txt){
+	int count = 0;
+	size_t i=0;
+	while( i < txt.length() ){
+		char c = txt.at(i);
+		std::string letterName = "";
+		if(c == '&'){
+			size_t k = i+1;
+			while( k < txt.length() && txt.at(k) != ';'){
+				k++;
+			}
+			i=k;
+		}
+		count++;
+		i++;
+	}
+	return count;
+}
