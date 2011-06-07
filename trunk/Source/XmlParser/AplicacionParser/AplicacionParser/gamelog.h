45ae539173511bc2dd5a7e023b4312c1195de984
aletorrado@gmail.com <aletorrado@gmail.com@df20077b-4ee2-056a-2025-6d81a732bcbf>
2011-06-07 01:54:47 +0000

    Correcciones de playersComparison

--- trunk/Source/XmlParser/AplicacionParser/AplicacionParser/gamelog.h
+++ trunk/Source/XmlParser/AplicacionParser/AplicacionParser/gamelog.h
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "database.h"
 #include "query.h"
 
@@ -14,11 +15,11 @@ public:
 	bool login(char* name);
 	bool createPlayer(char* name, char* password);
 	bool insertGame(char* player0, char* player1, int winner, int points0, int points1, int duration);
+	query* gamelog::getPlayers();
 	query* gamelog::playersByPlayedTime();
 	query* gamelog::playersByGamesPlayed();
 	query* gamelog::playersByTotalPoints();
 	query* gamelog::playersByWinnedCount();
 	query* gamelog::playersComparison(char* player0, char* player1);
-	query* getPlayers();
 	~gamelog(void);
 };
